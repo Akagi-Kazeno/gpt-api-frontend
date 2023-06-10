07927056a832b1b64d7bbbc606b73f1d4a7a8bb3
Akagi-Kazeno <akagikazeno@gmail.com>
2023-06-10 14:42:15 +0800

    menu bar

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
-
+import MenuBar from "../widgets/MenuBar.vue";
 </script>
 
 <template>
-
+  <el-container>
+    <el-header>
+      <menu-bar />
+    </el-header>
+    <el-main>
+      <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
+        <el-tab-pane label="Home" name="home">首页</el-tab-pane>
+        <el-tab-pane label="Article" name="article">作文批改</el-tab-pane>
+        <el-tab-pane label="MyArticle" name="my_article">我的作文</el-tab-pane>
+        <el-tab-pane label="Learn" name="learn">学习资料</el-tab-pane>
+      </el-tabs>
+    </el-main>
+  </el-container>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ src/components/widgets/MenuBar.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts"></script>
+
+<template>
+  <el-menu
+    :default-active="activeIndex"
+    class="el-menu-demo"
+    mode="horizontal"
+    :ellipsis="false"
+    @select="handleSelect"
+  >
+    <el-menu-item index="0">LOGO</el-menu-item>
+    <el-sub-menu index="1">
+      <template #title>
+        <el-avatar src="{{userAvatar}}"></el-avatar>
+      </template>
+      <el-menu-item class="center" index="/user/info">个人中心</el-menu-item>
+      <el-menu-item class="center" @click="quit">
+        <el-text type="danger">退出登录</el-text>
+      </el-menu-item>
+    </el-sub-menu>
+  </el-menu>
+</template>
+
+<style scoped lang="scss">
+.flex-grow {
+  flex-grow: 1;
+}
+
+.center {
+  text-align: center;
+}
+</style>
