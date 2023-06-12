3ea9916417535eb14a8282b9446e96d9722e95fb
Akagi-Kazeno <akagikazeno@gmail.com>
2023-06-12 13:18:49 +0800

    user service

--- src/components/widgets/HeadTabs.vue
+++ src/components/widgets/HeadTabs.vue
@@ -1,16 +1,23 @@
 <script setup lang="ts">
+import { ref } from "vue";
+import type { TabsPaneContext } from "element-plus";
 
+const activeName = ref("first");
+
+const handleClick = (tab: TabsPaneContext, event: Event) => {
+  console.log(tab, event);
+};
 </script>
 
 <template>
-  <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
-    <el-tab-pane label="Home" name="home">首页</el-tab-pane>
-    <el-tab-pane label="Article" name="article">作文批改</el-tab-pane>
-    <el-tab-pane label="MyArticle" name="my_article">我的作文</el-tab-pane>
-    <el-tab-pane label="Learn" name="learn">学习资料</el-tab-pane>
-  </el-tabs>
+  <div class="head-tabs">
+    <el-tabs v-model="activeName" class="head" @tab-click="handleClick">
+      <el-tab-pane label="Home" name="home">首页</el-tab-pane>
+      <el-tab-pane label="Article" name="article">作文批改</el-tab-pane>
+      <el-tab-pane label="MyArticle" name="my_article">我的作文</el-tab-pane>
+      <el-tab-pane label="Learn" name="learn">学习资料</el-tab-pane>
+    </el-tabs>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/components/widgets/MenuBar.vue
+++ src/components/widgets/MenuBar.vue
@@ -1,33 +1,41 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import instance from "../../interceptor/httpInterceptor.ts";
+import { getUserInfo, UserLogout } from "../../service/userService.ts";
 
 const activeIndex = ref("1");
 const handleSelect = (key: string, keyPath: string[]) => {
   console.log(key, keyPath);
 };
-const quit = instance.post("/user/quit");
+const quit = UserLogout;
+const user = await getUserInfo().then((res) => {
+  const userAvatar = res.avatar;
+  return {
+    userAvatar,
+  };
+});
 </script>
 
 <template>
-  <el-menu
-    :default-active="activeIndex"
-    class="el-menu-demo"
-    mode="horizontal"
-    :ellipsis="false"
-    @select="handleSelect"
-  >
-    <el-menu-item index="0">LOGO</el-menu-item>
-    <el-sub-menu index="1">
-      <template #title>
-        <el-avatar src="{{userAvatar}}"></el-avatar>
-      </template>
-      <el-menu-item class="center" index="/user/info">个人中心</el-menu-item>
-      <el-menu-item class="center" @click="quit">
-        <el-text type="danger">退出登录</el-text>
-      </el-menu-item>
-    </el-sub-menu>
-  </el-menu>
+  <div class="menu-bar">
+    <el-menu
+      :default-active="activeIndex"
+      class="menu"
+      mode="horizontal"
+      :ellipsis="false"
+      @select="handleSelect"
+    >
+      <el-menu-item index="0">LOGO</el-menu-item>
+      <el-sub-menu index="1">
+        <template #title>
+          <el-avatar src="{{user.userAvatar}}"></el-avatar>
+        </template>
+        <el-menu-item class="center" index="/user/info">个人中心</el-menu-item>
+        <el-menu-item class="center" @click="quit">
+          <el-text type="danger">退出登录</el-text>
+        </el-menu-item>
+      </el-sub-menu>
+    </el-menu>
+  </div>
 </template>
 
 <style scoped lang="scss">
